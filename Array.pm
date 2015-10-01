a1f171213adbc13083e347559e616e2b7d9b1406
Michal Špaček <tupinek@gmail.com>
2015-10-01 12:43:00 +0200

    Prepare for 0.06 version.

--- Array.pm
+++ Array.pm
@@ -15,7 +15,7 @@ use Readonly;
 Readonly::Scalar my $EMPTY_STR => q{};
 
 # Version.
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -409,6 +409,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
