f31bb16c5d695d8249b44a86afb0f9e403140d61
Michal Josef Špaček <tupinek@gmail.com>
2023-11-22 23:11:25 +0100

    Improve LICENSE AND COPYRIGHT section in doc

--- Array.pm
+++ Array.pm
@@ -394,8 +394,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2011-2018 Michal Josef Špaček
- BSD 2-Clause License
+© 2011-2018 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
